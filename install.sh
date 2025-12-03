e79d72a7af3c5065bfc10a93a6b1f365740e4e3e
Matteo De Pellegrin <matteo.dep97@gmail.com>
2025-12-03 11:59:30 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,34 +1,44 @@
 #!/bin/sh
 
-sudocmd=
+SUDOCMD=sudo
+GPGHOME="$HOME/.config/gnupg"
 
 recurse(){
 	# shellcheck disable=SC2045
 	for f in $(ls -A "$1"); do
 		n=0
-		indent=
-		while [ "$n" -lt "$3" ]; do
-			indent="$indent  "
-			n="$(( n + 1 ))"
-		done
+		indent=$3
 		if [ -d "$1/$f" ]; then
-			confirm "mkdir -p $1/$f"
-			recurse "$1/$f" "$2/$f" "$(( $3 + 1 ))"
+			echo recursing "$1/$f" "$2/$f" "  $3"
+			recurse "$1/$f" "$2/$f" "  $3"
+		elif [ -L "$2/$f" ] && [ "$(realpath "$2/$f")" = "$1/$f" ]; then
+			printf 'file %s is already tracked.\n' "$2/$f"
 		else
-			confirm "ln -srf $1/$f $2/$f"
+			confirm "Would you like to sync $2/$f?"
+			sync "$1" "$2" "$f"
 		fi
 	done
 }
 
-confirm(){
+run(){
 	cmd="$1"
-	[ "$sudocmd" ] && cmd="$sudocmd $cmd"
+	[ "$USE_SUDO" ] && cmd="$SUDOCMD $cmd"
+	$cmd
+}
+
+sync(){
+	if [ ! -d "$2" ]; then
+		run "mkdir -p $2"
+	fi
+	run "ln -srf $1/$3 $2/$3"
+}
+
+confirm(){
 	while printf "%s? [Y/n] " "$indent$1"; do
 		read -r ans
-		if echo "$ans" | grep -iq '^no\?$'; then
+		if echo "$ans" | grep -iq '^\(n\|no\)$'; then
 			break
-		elif echo "$ans" | grep -iq '^y\?e\?s\?$'; then
-			$cmd
+		elif echo "$ans" | grep -iq '^\(y\|yes\)$'; then
 			break
 		else
 			printf "\nCouldn't parse %s.\n" "$ans"
@@ -36,14 +46,12 @@ confirm(){
 	done
 }
 
-recurse "$PWD/home" "$HOME" 0
+recurse "$PWD/home" "$HOME"
+USE_SUDO=1
+recurse "$PWD/root" ""
 
-sudocmd=sudo
-recurse "$PWD/root" "/" 0
-
-gpghome="$HOME/.config/gnupg"
-if [ -d "$gpghome" ]; then
-	sudo chown "$(whoami)" "$gpghome"
-	find "$gpghome" -type f -exec chmod 600 {} \;
-	find "$gpghome" -type d -exec chmod 700 {} \;
+if [ -d "$GPGHOME" ]; then
+	sudo chown "$(whoami)" "$GPGHOME"
+	find "$GPGHOME" -type f -exec chmod 600 {} \;
+	find "$GPGHOME" -type d -exec chmod 700 {} \;
 fi
